d8d8c7ce69d277d3bfa93429640fcc14e19da458
RumeysaTuysuz <rumeysatysz2002@gmail.com>
2026-01-30 21:15:51 +0300

    Add project files.

---
+++ SmartAppointment.API/Data/SmartAppointmentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartAppointment.API.Data
+{
+	public class SmartAppointmentDbContext : DbContext
+	{
+		public SmartAppointmentDbContext(DbContextOptions<SmartAppointmentDbContext> options)
+			: base(options)
+		{
+		}
+
+		// public DbSet<Appointment> Appointments { get; set; }
+	}
+}
