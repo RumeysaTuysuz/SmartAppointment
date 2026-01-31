ca6c680939e02f3a2773b8af7ce094a8dd02a9de
RumeysaTuysuz <rumeysatysz2002@gmail.com>
2026-02-01 00:05:36 +0300

    Add AppointmentsController base structure

---
+++ SmartAppointment.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartAppointment.API.Data;
+using SmartAppointment.API.Models;
+
+namespace SmartAppointment.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class AppointmentsController : ControllerBase
+	{
+		private readonly SmartAppointmentDbContext _context;
+
+		public AppointmentsController(SmartAppointmentDbContext context)
+		{
+			_context = context;
+		}
+	}
+}
