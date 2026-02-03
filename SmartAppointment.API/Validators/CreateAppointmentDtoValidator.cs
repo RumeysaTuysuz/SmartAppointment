52c975b450b00e5520b49efc759f7bcab06347c6
RumeysaTuysuz <rumeysatysz2002@gmail.com>
2026-02-04 01:41:26 +0300

    Add CreateAppointmentDto validator

---
+++ SmartAppointment.API/Validators/CreateAppointmentDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SmartAppointment.API.Dtos;
+
+namespace SmartAppointment.API.Validators
+{
+	public class CreateAppointmentDtoValidator
+		: AbstractValidator<CreateAppointmentDto>
+	{
+		public CreateAppointmentDtoValidator()
+		{
+			RuleFor(x => x.FullName)
+				.NotEmpty()
+				.MinimumLength(3);
+
+			RuleFor(x => x.Email)
+				.NotEmpty()
+				.EmailAddress();
+
+			RuleFor(x => x.AppointmentDate)
+				.GreaterThan(DateTime.Now);
+
+			RuleFor(x => x.Description)
+				.MaximumLength(500);
+		}
+	}
+}
